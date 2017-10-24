77521052c699e4147d7bacbcea2c3df5fc6f2823
laetitiatureau <laetitia.tureau@univ-tlse3.fr>
2017-10-24 09:14:02 +0200

    commit ItemTest

---
+++ ItemTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test Unitaire de la classe Item
+ */
+public class ItemTest {
+    Item item = new Item(4);
+
+    @Test
+    public void getItem() throws Exception {
+        assertThat(item, item.getItem());
+    }
+
+    @Test
+    public void getValue() throws Exception {
+        assertThat(4, item.getValue());
+    }
+
+    @Test
+    public void setItem() throws Exception {
+        Item it = new Item(5);
+        assertThat(it, sameInstance(item.setItem(it)));
+        assertThat(it.getItem(), item.getItem());
+        assertThat(it.getValue(), item.getValue());
+    }
+    //tester les erreurs TODO
+}
